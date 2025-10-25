e1a8fdc2485de979d84c15c5e2070fd3413b5aa8
shafiqul8 <shafiqul.cse.it@gmail.com>
2025-10-25 15:51:32 +0600

    Update utils.py

--- iris_flower_classifier/src/utils.py
+++ iris_flower_classifier/src/utils.py
@@ -1,1 +1,10 @@
 
+# src/utils.py
+import pandas as pd
+from sklearn.datasets import load_iris
+
+def load_iris_dataset():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df["species"] = iris.target
+    return df
