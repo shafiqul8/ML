440adba4ae8240e5911c63fb47e4147f5c89af91
shafiqul8 <shafiqul.cse.it@gmail.com>
2025-10-25 15:52:03 +0600

    Update predictor.py

--- iris_flower_classifier/src/predictor.py
+++ iris_flower_classifier/src/predictor.py
@@ -1,1 +1,14 @@
 
+# src/predictor.py
+import joblib
+from sklearn.datasets import load_iris
+
+def predict_species(features, model_path):
+    """
+    features: list of 4 numeric values (sepal length, sepal width, petal length, petal width)
+    """
+    model = joblib.load(model_path)
+    iris = load_iris()
+    prediction = model.predict([features])[0]
+    species = iris.target_names[prediction]
+    return species
