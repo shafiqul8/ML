8b0397c20740c50bd2308c6e2f9de03edf5097de
shafiqul8 <shafiqul.cse.it@gmail.com>
2025-10-25 16:34:06 +0600

    Update predictor.py

--- music_recommender/src/predictor.py
+++ music_recommender/src/predictor.py
@@ -2,13 +2,12 @@
 
 # src/predictor.py
 import joblib
+import pands as pd
 
 def predict_species(features, model_path):
     """
     features: list of 4 numeric values (sepal length, sepal width, petal length, petal width)
     """
     model = joblib.load(model_path)
-    iris = load_iris()
     prediction = model.predict([features])[0]
-    species = iris.target_names[prediction]
-    return species
+    return prediction
